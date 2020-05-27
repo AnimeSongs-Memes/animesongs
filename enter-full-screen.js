34f5b3996b709cebda275456d202d8cf6d7ef649
AnimeSongs-Memes <63086294+AnimeSongs-Memes@users.noreply.github.com>
2020-05-27 15:52:37 -0400

    4.0
    Adds fullscreen

---
+++ enter-full-screen.js
@@ -0,0 +1,15 @@
+function EnterFull() {
+var elem = document.querySelector('video');
+	if(document.querySelector('video').playing){ // checks if element is playing right now
+    if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+}

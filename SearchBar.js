dc2d04215dff71d56760ceb1006148b51bd0d7f9
AnimeSongs-Memes <63086294+AnimeSongs-Memes@users.noreply.github.com>
2020-05-29 10:36:05 -0400

    Search bar fix

--- SearchBar.js
+++ SearchBar.js
@@ -1,9 +1,6 @@
 function myFunction1() {
   var x = document.getElementById("myText1").value;
   find(x);
-   if (window.getSelection) {window.getSelection().removeAllRanges();}
- else if (document.selection) {document.selection.empty();}
-
 }
 
 function TestEnter(event) {

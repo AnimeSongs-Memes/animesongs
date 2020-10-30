87516bd3e3dd3dabd980f2f8ad5d18f199c761f3
AnimeSongs-Memes <63086294+AnimeSongs-Memes@users.noreply.github.com>
2020-10-29 22:58:31 -0400

    improved search bar

--- SearchBar.js
+++ SearchBar.js
@@ -1,10 +1,15 @@
 function myFunction1() {
-  var x = document.getElementById("myText1").value;
-  find(x);
+    document.getElementById("myText1").focus();
+    var x = document.getElementById("myText1").value;
+    find(x);
+    window.getSelection().anchorNode.parentElement.parentElement.parentElement.parentElement.scrollIntoView({
+        block: "center"
+    });
+    window.getSelection().removeAllRanges();
 }
 
 function TestEnter(event) {
-   if (window.event.keyCode == 13) {
-myFunction1();
-   }
+    if (window.event.keyCode == 13) {
+        myFunction1();
+    }
 }
